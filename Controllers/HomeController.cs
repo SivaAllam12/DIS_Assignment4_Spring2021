b9f76438384c7ba05e0e263ea7fdbbcf9e10d6c7
Sivaramakrishna Allam <siva.allam94@gmail.com>
2021-04-21 00:19:04 -0400

    fetch API data and display

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using DIS_Assignment4_Spring2021.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DIS_Assignment4_Spring2021.Controllers
+{
+    public class HomeController : Controller
+    {
+        static string api_link = "https://data.cdc.gov/resource/hk9y-quqm.json?$limit=2&state=Florida";
+
+        HttpClient httpclient = new HttpClient();
+
+        Covid_Conditions covid_conditions = new Covid_Conditions();
+        public IActionResult Data()
+        {
+            httpclient.BaseAddress = new Uri(api_link);
+
+            HttpResponseMessage response = httpclient.GetAsync(api_link).GetAwaiter().GetResult();
+
+            string covidData = null;
+
+            //var responseTask = httpclient.GetAsync(api_link);
+            //responseTask.Wait();
+            //var result = responseTask.Result;
+            if (response.IsSuccessStatusCode)
+            {
+                covidData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            }
+            if (!covidData.Equals(""))
+            {
+                // JsonConvert is part of the NewtonSoft.Json Nuget package
+                covid_conditions.data = JsonConvert.DeserializeObject<List<Covid_Condition>>(covidData);
+            }
+            return View(covid_conditions);
+        }
+    }
+}
+
+
+
+
---
+++ Models/Covid_Condition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIS_Assignment4_Spring2021.Models
+{
+        public class Covid_Conditions
+        {
+            public List<Covid_Condition> data { get; set; }
+        }
+
+        public class Covid_Condition
+        {
+            public DateTime data_as_of { get; set; }
+            public DateTime start_date { get; set; }
+            public DateTime end_date { get; set; }
+            public string group { get; set; }
+            public string state { get; set; }
+            public string condition_group { get; set; }
+            public string condition { get; set; }
+            public string icd10_codes { get; set; }
+            public string age_group { get; set; }
+            public string covid_19_deaths { get; set; }
+            public string number_of_mentions { get; set; }
+            public string flag { get; set; }
+            public Covid_Conditions covid_conditions { get; set; }
+        }
+ }
+
